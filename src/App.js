455863296d882a20feed77eb6875296176ec6b46
Bruno <bruno_151997@hotmail.com>
2022-01-03 19:47:37 -0300

    Estrutura inicial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Login from "./components/Login/Login";
 
 function App() {
   return (
-    <div>
-      <p>aasd</p>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
